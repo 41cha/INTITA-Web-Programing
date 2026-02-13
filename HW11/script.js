f7719a4d116763453dc710fa3be8e697636c9981
41cha <vovaa.dzimina@gmail.com>
2026-02-13 09:58:48 +0200

    fix HW 11

--- HW11/script.js
+++ HW11/script.js
@@ -2,20 +2,6 @@ let btn = document.getElementById("btn");
 let text = document.getElementById("text");
 
 btn.addEventListener("click", function () {
-    text.textContent = "Текст змінився!";
-});
-
-btn.addEventListener("click", function () {
-    text.textContent = "Текст став червоним!";
-    text.style.color = "red";
-});
-
-btn.addEventListener("click", function () {
-    text.classList.toggle("dark");
-});
-
-
-document.getElementById("btn").onclick = () => {
+    text.classList.toggle("red-text");
     document.body.classList.toggle("dark");
-};
-
+});
